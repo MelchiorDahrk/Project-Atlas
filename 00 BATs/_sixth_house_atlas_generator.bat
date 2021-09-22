4269a51bf8e90ff1e3b2d223c25de92bb0337989
revenorror <78823150+revenorror@users.noreply.github.com>
2021-09-22 10:49:52 -0500

    Sixth House atlas generator

---
+++ 00 BATs/_sixth_house_atlas_generator.bat
@@ -0,0 +1,30 @@
+@echo off
+if not exist ATL mkdir ATL
+for /f %%i in ('magick convert tx_metal_6th_bells_top.dds -format %%w info:') do set resolutionW=%%i
+set /A resolution2=%resolutionW%*2
+set /A resolutionH=%resolution2%*4
+
+magick convert tx_glass_6th_blackblock.dds tx_glass_6th_blackblock.dds +append ATL/temp1.bmp
+magick convert ATL/temp1.bmp ATL/temp1.bmp -append ATL/temp2.bmp
+magick convert ATL/temp2.bmp tx_metal_6th_design.dds +append ATL/temp3.bmp
+magick convert tx_glass_6th_jewel.dds tx_wood_6th_redblock.dds +append ATL/temp4.bmp
+magick convert tx_metal_6th_strip.dds tx_metal_6th_strip.dds +append ATL/temp5.bmp
+magick convert tx_wood_6th_redblock_01.dds tx_wood_6th_post.dds -rotate 90 -append ATL/temp6.bmp
+
+magick convert tx_metal_6th_top.dds tx_metal_6th_bells_top.dds tx_metal_6th_bells.dds tx_glass_obsidian.dds ATL/temp3.bmp ATL/temp4.bmp ATL/temp5.bmp ATL/temp5.bmp tx_stone_6th_trim_01.dds tx_metal_6th_strip_01.dds tx_metal_6th_strip_01.dds tx_wood_6th_blacktrim.dds tx_wood_6th_redtrim_01.dds tx_wood_6th_redtrim.dds ATL/temp6.bmp -append -resize %resolutionW% -define dds:compression=dxt1 ATL/atlad_6th.dds
+
+magick convert tx_ash_firepit.dds tx_ash_firepit.dds +append ATL/temp7.bmp
+
+magick convert tx_glass_6th_ashaltar.dds tx_stone_6th_ashstatue.dds ATL/temp7.bmp tx_glass_obsidian_strip.dds tx_stone_6th_trim_02.dds -background "#312c28" -append -resize %resolution2% -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlad_6th01.dds
+
+cd ATL
+del temp1.bmp
+del temp2.bmp
+del temp3.bmp
+del temp4.bmp
+del temp5.bmp
+del temp6.bmp
+del temp7.bmp
+cd ...
+
+if not defined run_without_pause pause
