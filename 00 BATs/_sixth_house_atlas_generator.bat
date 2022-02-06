63aa21f3c8b9fa2a25ffd3bd89d3da1072149b51
revenorror <78823150+revenorror@users.noreply.github.com>
2022-02-06 09:41:58 -0600

    Updated 6th house BAT

--- 00 BATs/_sixth_house_atlas_generator.bat
+++ 00 BATs/_sixth_house_atlas_generator.bat
@@ -14,8 +14,9 @@ magick convert tx_wood_6th_redblock_01.dds tx_wood_6th_post.dds -rotate 90 -appe
 magick convert tx_metal_6th_top.dds tx_metal_6th_bells_top.dds tx_metal_6th_bells.dds tx_glass_obsidian.dds ATL/temp3.bmp ATL/temp4.bmp ATL/temp5.bmp ATL/temp5.bmp tx_stone_6th_trim_01.dds tx_metal_6th_strip_01.dds tx_metal_6th_strip_01.dds tx_wood_6th_blacktrim.dds tx_wood_6th_redtrim_01.dds tx_wood_6th_redtrim.dds ATL/temp6.bmp -append -resize %resolutionW% -define dds:compression=dxt1 ATL/atlad_6th.dds
 
 magick convert tx_ash_firepit.dds tx_ash_firepit.dds +append ATL/temp7.bmp
+magick convert tx_stone_6th_ashstatue.dds -rotate -90 +append ATL/temp8.bmp
 
-magick convert tx_glass_6th_ashaltar.dds tx_stone_6th_ashstatue.dds tx_stone_6th_ashstatue.dds ATL/temp7.bmp tx_glass_obsidian_strip.dds tx_stone_6th_trim_02.dds -background "#312c28" -append -resize %resolution2% -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlad_6th01.dds
+magick convert tx_glass_6th_ashaltar.dds ATL/temp8.bmp ATL/temp7.bmp tx_glass_obsidian_strip.dds tx_stone_6th_trim_02.dds -background "#312c28" -append -resize %resolution2% -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlad_6th01.dds
 
 cd ATL
 del temp1.bmp
@@ -25,6 +26,7 @@ del temp4.bmp
 del temp5.bmp
 del temp6.bmp
 del temp7.bmp
+del temp8.bmp
 cd ..
 
 if not defined run_without_pause pause
