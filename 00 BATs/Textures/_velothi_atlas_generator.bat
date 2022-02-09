00e02ca59ac04607d0d7fda2ed9789bcce1faf09
revenorror <78823150+revenorror@users.noreply.github.com>
2022-02-08 22:19:21 -0600

    Rewrote velothi bat

--- 00 BATs/Textures/_velothi_atlas_generator.bat
+++ 00 BATs/Textures/_velothi_atlas_generator.bat
@@ -13,17 +13,21 @@ IF /I %resizeLevel% GEQ 1 (
 if not exist ATL mkdir ATL
 for /f %%i in ('magick convert Tx_V_floor_01.dds -format %%w info:') do set resolutionW=%%i
 set /A resolutionW=%resolutionW%/%resize%
-set /A resolutionH=%resolutionW%*16
+set /A resolutionH=%resolutionW%*8
 
 magick convert Tx_block_adobe_redbrown_01.dds Tx_block_adobe_redbrown_01.dds +append ATL/temp1.bmp
+
 for /f %%i in ('magick convert Tx_V_bridgedetail_04.dds -format %%h info:') do set resolutionBH=%%i
 for /f %%i in ('magick convert Tx_V_bridgedetail_04.dds -format %%w info:') do set resolutionBW=%%i
 if "%resolutionBH%" NEQ "%resolutionBW%" (
 	magick convert Tx_V_bridgedetail_04.dds -define dds:compression=dxt1 -gravity north -background black -extent x%resolutionBW% Tx_V_bridgedetail_04.dds
 )
-
 magick convert Tx_V_bridgedetail_04.dds Tx_V_bridgedetail_04.dds +append ATL/temp2.bmp
-magick convert tx_v_bridgedetail_02.dds ATL/temp1.bmp ATL/temp2.bmp Tx_V_floor_01.dds Tx_V_entcover_01.dds Tx_V_bridgedetail_03.dds Tx_V_bridgedetail_01.dds Tx_V_base_10.dds Tx_V_base_09.dds Tx_V_base_07.dds Tx_V_base_04.dds Tx_V_base_02.dds Tx_V_base_01.dds Tx_V_strip_02.dds Tx_V_strip_04.dds Tx_V_strip_03.dds Tx_V_strip_01.dds Tx_V_base_08.dds Tx_wall_adobe_brown_02.dds Tx_V_B_base_01.dds -resize %resolutionW% -append -gravity south -background black -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlas_velothi.dds
+
+magick convert tx_v_base_08.dds tx_v_base_01.dds tx_wall_adobe_brown_02.dds ATL/temp1.bmp tx_v_strip_01.dds tx_v_b_base_01.dds tx_v_strip_04.dds tx_v_strip_03.dds tx_v_floor_01.dds ATL/temp2.bmp tx_v_bridgedetail_03.dds tx_v_bridgedetail_02.dds tx_v_bridgedetail_01.dds -resize %resolutionW% -append -gravity south -background black -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlas_velothi_01.dds
+
+magick convert tx_v_strip_02.dds tx_v_base_09.dds tx_v_base_07.dds tx_v_base_04.dds tx_v_base_02.dds tx_v_base_10.dds tx_v_entcover_01.dds -resize %resolutionW% -append -gravity south -background black -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlas_velothi_02.dds
+
 cd ATL
 del "temp1.bmp"
 del "temp2.bmp"
