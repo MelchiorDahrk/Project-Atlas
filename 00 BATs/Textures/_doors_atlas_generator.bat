e937470a62a4eec1816d60d2e6ca7dbfde9ff505
revenorror <78823150+revenorror@users.noreply.github.com>
2022-02-08 23:16:03 -0600

    Added MET patch

--- 00 BATs/Textures/_doors_atlas_generator.bat
+++ 00 BATs/Textures/_doors_atlas_generator.bat
@@ -5,12 +5,14 @@ set /A resolutionH=%resolutionW%*8
 
 magick convert tx_jail_door_01.dds -rotate 90 -append ATL/temp1.bmp
 magick convert tx_metal_hlaalulock.dds tx_keyhole_05.dds +append ATL/temp2.bmp
+magick convert tx_hlaalu_doorplank_01.dds -resize %resolutionW% -append ATL/temp3.bmp
 
-magick convert ATL/temp1.bmp tx_hinge_02.dds ATL/temp2.bmp tx_metal_strip_02.dds tx_velothi_doorstrip_01.dds tx_hlaalu_doorplank_01.dds tx_velothi_door_01.dds tx_hlaalu_door_02.dds tx_hlaalu_door_01.dds -gravity southwest -background "#26261b" -append -resize %resolutionW% -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlas_doors_01.dds
+magick convert ATL/temp1.bmp tx_hinge_02.dds ATL/temp2.bmp tx_metal_strip_02.dds tx_velothi_doorstrip_01.dds ATL/temp3.bmp tx_velothi_door_01.dds tx_hlaalu_door_02.dds tx_hlaalu_door_01.dds -gravity southwest -background "#26261b" -append -resize %resolutionW% -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlas_doors_01.dds
 
 cd ATL
 del "temp1.bmp"
 del "temp2.bmp"
+del "temp3.bmp"
 cd ..
 
 if not defined run_without_pause pause
--- 00 BATs/Textures/_sixth_house_atlas_generator.bat
+++ 00 BATs/Textures/_sixth_house_atlas_generator.bat
@@ -11,12 +11,12 @@ magick convert tx_glass_6th_jewel.dds tx_wood_6th_redblock.dds +append ATL/temp4
 magick convert tx_metal_6th_strip.dds tx_metal_6th_strip.dds +append ATL/temp5.bmp
 magick convert tx_wood_6th_redblock_01.dds tx_wood_6th_post.dds -rotate 90 -append ATL/temp6.bmp
 
-magick convert tx_metal_6th_top.dds tx_metal_6th_bells_top.dds tx_metal_6th_bells.dds tx_glass_obsidian.dds ATL/temp3.bmp ATL/temp4.bmp ATL/temp5.bmp ATL/temp5.bmp tx_stone_6th_trim_01.dds tx_metal_6th_strip_01.dds tx_metal_6th_strip_01.dds tx_wood_6th_blacktrim.dds tx_wood_6th_redtrim_01.dds tx_wood_6th_redtrim.dds ATL/temp6.bmp -append -resize %resolutionW% -define dds:compression=dxt1 ATL/atlad_6th.dds
+magick convert tx_metal_6th_top.dds tx_metal_6th_bells_top.dds tx_metal_6th_bells.dds tx_glass_obsidian.dds ATL/temp3.bmp ATL/temp4.bmp ATL/temp5.bmp ATL/temp5.bmp tx_stone_6th_trim_01.dds tx_metal_6th_strip_01.dds tx_metal_6th_strip_01.dds tx_wood_6th_blacktrim.dds tx_wood_6th_redtrim_01.dds tx_wood_6th_redtrim.dds ATL/temp6.bmp -resize %resolutionW% -append -define dds:compression=dxt1 ATL/atlad_6th.dds
 
 magick convert tx_ash_firepit.dds tx_ash_firepit.dds +append ATL/temp7.bmp
 magick convert tx_stone_6th_ashstatue.dds -rotate -90 +append ATL/temp8.bmp
 
-magick convert tx_glass_6th_ashaltar.dds ATL/temp8.bmp ATL/temp7.bmp tx_glass_obsidian_strip.dds tx_stone_6th_trim_02.dds -background "#312c28" -append -resize %resolution2% -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlad_6th01.dds
+magick convert tx_glass_6th_ashaltar.dds ATL/temp8.bmp ATL/temp7.bmp tx_glass_obsidian_strip.dds tx_stone_6th_trim_02.dds -background "#312c28" -resize %resolution2% -append -extent x%resolutionH% -define dds:compression=dxt1 ATL/atlad_6th01.dds
 
 cd ATL
 del temp1.bmp
